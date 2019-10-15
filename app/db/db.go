80f30e196c044807b7b4e3269b7f3f62f0d880be
Remap Development <mohammadrizkyk@gmail.com>
2019-10-15 17:13:08 +0700

    db sqlite

--- app/db/db.go
+++ app/db/db.go
@@ -1,41 +1,46 @@
 package db
 
 import (
-	"github.com/heroku/go-getting-started/app/config"
-	"github.com/heroku/go-getting-started/app/model"
-	"fmt"
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/mysql"
+	//"fmt"
+	//"github.com/heroku/go-getting-started/app/model"
+
+	//"github.com/heroku/go-getting-started/app/config"
+	//"github.com/heroku/go-getting-started/app/model"
+	//"fmt"
+	//"github.com/jinzhu/gorm"
+	//_ "github.com/jinzhu/gorm/dialects/mysql"
+	//"github.com/heroku/go-getting-started/app/config"
+	//"github.com/jinzhu/gorm"
 )
 
-var db *gorm.DB
-var err error
+//var db *gorm.DB
+//var err error
 
 func Init() {
-	configuration := config.GetConfig()
-	connect_string := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
-		configuration.DB_USERNAME,
-		configuration.DB_PASSWORD,
-		configuration.DB_HOST,
-		configuration.DB_PORT,
-		configuration.DB_NAME)
-	fmt.Println(connect_string)
-	db, err = gorm.Open("mysql", connect_string)
-
-	if err != nil {
-		panic("DB Connection Error")
-	}
-	migrate()
+	//configuration := config.GetConfig()
+	//connect_string := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
+	//	configuration.DB_USERNAME,
+	//	configuration.DB_PASSWORD,
+	//	configuration.DB_HOST,
+	//	configuration.DB_PORT,
+	//	configuration.DB_NAME)
+	//fmt.Println(connect_string)
+	//db, err = gorm.Open("mysql", connect_string)
+	//
+	//if err != nil {
+	//	panic("DB Connection Error")
+	//}
+	//migrate()
 }
-
-func migrate() {
-	db.Model(&model.User{}).DropColumn("address")
-	db.AutoMigrate(&model.User{})
-	db.AutoMigrate(&model.Authentication{})
-	db.Model(&model.Authentication{}).AddForeignKey("user_id",
-		"users(id)", "CASCADE", "CASCADE")
-}
-
-func DbManager() *gorm.DB {
-	return db
-}
+//
+//func migrate() {
+//	db.Model(&model.User{}).DropColumn("address")
+//	db.AutoMigrate(&model.User{})
+//	db.AutoMigrate(&model.Authentication{})
+//	db.Model(&model.Authentication{}).AddForeignKey("user_id",
+//		"users(id)", "CASCADE", "CASCADE")
+//}
+//
+//func DbManager() *gorm.DB {
+//	return db
+//}
